f876dcb8ec60d82a5c475c0e7199592d5bcd84ac
priyamdavra <priyamdavra@gmail.com>
2025-01-23 11:48:11 +0530

    retrive data from form.

--- JS_DOM/app.js
+++ JS_DOM/app.js
@@ -1,12 +1,11 @@
-let btn = document.querySelector("button");
-let h1 = document.querySelector("h1");
-let h3 = document.querySelector("h3");
+let form = document.querySelector("form");
 
-function changeColor () {
-    console.dir(this.innerText);
-    this.style.backgroundColor = "blue";
-}
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let user = document.querySelector("#user");
+    let pass = document.querySelector("#pass");
+     
+    console.log(user.value);
+    console.log(pass.value);
+});
 
-btn.addEventListener("click", changeColor);
-h1.addEventListener("click", changeColor);
-h3.addEventListener("click", changeColor);
